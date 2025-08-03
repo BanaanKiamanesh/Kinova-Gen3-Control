b99dfe8a6171444e31642a47794b124d3dede208
Banaan Kiamanesh <kiamaneshbanaan@gmail.com>
2025-08-03 23:43:53 +0200

    Adding Task Space Trajectory
    Adding the Task Space Trajectory + Solving the solver Issues

--- 3. Control - Feedback Linearization + PD/Design.m
+++ 3. Control - Feedback Linearization + PD/Design.m
@@ -12,3 +12,14 @@ qInitDM = [zeros(14, 1)];
 
 % Simscape Multi-Body Model
 qInitSM = [zeros(14, 1)];
+
+%% Derivative Filter(High Pass as Smooth Derivative)
+W = 2 * pi * 50;
+Zeta = 1 / sqrt(2);
+
+DF.num = [W^2, 0];
+DF.den = [1, 2*Zeta*W, W^2];
+
+%% Load Robot for Inverse Kinematics
+robot = loadrobot('kinovaGen3');
+robot.DataFormat = 'column';
