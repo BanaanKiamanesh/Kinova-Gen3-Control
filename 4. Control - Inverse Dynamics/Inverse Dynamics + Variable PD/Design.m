2c7bf60443fa6f447f9defab6e48d94e23503165
Banaan Kiamanesh <kiamaneshbanaan@gmail.com>
2025-08-04 00:21:13 +0200

    Adding the Task Space Trajectory

--- 4. Control - Inverse Dynamics/Inverse Dynamics + Variable PD/Design.m
+++ 4. Control - Inverse Dynamics/Inverse Dynamics + Variable PD/Design.m
@@ -12,7 +12,18 @@ KdSM = 500000;
 
 %% Initial Conditions
 % Dynamical Model
-qInitDM = [rand(7, 1); zeros(7, 1)];
+qInitDM = [zeros(14, 1)];
 
 % Simscape Multi-Body Model
-qInitSM = [rand(7, 1); zeros(7, 1)];
+qInitSM = [zeros(14, 1)];
+
+%% Derivative Filter(High Pass as Smooth Derivative)
+W = 2 * pi * 50;
+Zeta = 1 / sqrt(2);
+
+DF.num = [W^2, 0];
+DF.den = [1, 2*Zeta*W, W^2];
+
+%% Load Robot for Inverse Kinematics
+robot = loadrobot('kinovaGen3');
+robot.DataFormat = 'column';
